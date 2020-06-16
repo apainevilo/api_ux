462b852f561a9fdd87b568aaa572d5e517dcf213
alonso painevilo <alonso.desarrollador@gmail.com>
2020-06-16 17:58:49 -0400

    Second

--- api_ux/api_ux/settings.py
+++ api_ux/api_ux/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hdr1l8y9h$c7m$(c_7z#s&c$^4axadwd5u3o(ex6fp0rmu#17)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
